a12181115e8e0b5d1d8d9119e9f70dbf860400f7
Will <programergeek@gmail.com>
2023-09-17 14:50:26 +0200

    feat: get all users and add user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,44 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const supabase = require("../supabase/supabase");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", async function (req, res, next) {
+  try {
+    const { data, error } = await supabase.from("users").select("*");
+    if (error) {
+      console.log(error);
+      res.send({ message: "Fail", status: 513 });
+    }
+    res.send({ message: data, status: 200 });
+  } catch (e) {
+    console.log("Failed to retrieve users from database:\n " + e);
+    res.send({ message: "Fail", status: 505 });
+  }
 });
+
+// Add a new user
+router.post("/add", async (req, res, next) => {
+  const user = req.body;
+  try {
+    const { data, error } = await supabase.from("users").insert([
+      {
+        name: user.name,
+        age: user.age,
+        address: user.address,
+      },
+    ]);
+    if (error) {
+      console.log("Supabase: " + error);
+      res.send({ message: "Database insert error.", status: 514 });
+    }
+    res.send({ message: "Success", status: 200 });
+  } catch (e) {
+    console.error("Database request error: " + e);
+    res.send({ message: "Failed", status: 500 });
+  }
+});
+
+//
 
 module.exports = router;
