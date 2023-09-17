d5629e5c32db84cf971d69dfc5fb029584e0410e
Will <programergeek@gmail.com>
2023-09-17 14:49:43 +0200

    fix: dotenv init

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-const dotenv = require('dotenv')
-
-dotenv.config()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
